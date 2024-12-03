5a3804d301a5434b353a2ee8bafad17897e172e7
Ireme-Nyawe <akimana.inono@gmail.com>
2024-12-03 20:55:23 +0200

    change job time to a day

--- src/database/cron/announcement.js
+++ src/database/cron/announcement.js
@@ -1,19 +1,23 @@
 import cron from "node-cron";
 import announcementRepository from "../../modules/annoucement/repository/announcementRepository.js";
+import { sendEmail } from "../../services/sendEmail.js";
 
 export const scheduleUpdateAnnouncementDues = () => {
-  cron.schedule("* * * * *", async () => {
+  cron.schedule("0 0 * * *", async () => {
     try {
-        const now = new Date();
-      const dueAnnouncements = await announcementRepository.findDueAnnouncement(now);
+      const now = new Date();
+      const dueAnnouncements = await announcementRepository.findDueAnnouncement(
+        now
+      );
 
       if (dueAnnouncements.length > 0) {
-        const announcementIds = dueAnnouncements.map((announcement) => announcement._id);
-        await announcementRepository.updateDueAnnouncementStatus(announcementIds);
-
-        console.log(`Updated ${announcementIds.length} records to "unPublished".`);
-      } else {
-        console.log("No records to update.");
+        const announcementIds = dueAnnouncements.map(
+          (announcement) => announcement._id
+        );
+        await announcementRepository.updateDueAnnouncementStatus(
+          announcementIds
+        );
+       
       }
     } catch (error) {
       console.error("Error in cron job:", error);
