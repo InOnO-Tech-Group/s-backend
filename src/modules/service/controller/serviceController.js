58526c5674488ac73adb4ade7783ec60df348f80
Ireme-Nyawe <akimana.inono@gmail.com>
2024-12-04 09:53:35 +0200

    add controller for viewing services

--- src/modules/service/controller/serviceController.js
+++ src/modules/service/controller/serviceController.js
@@ -1,25 +1,50 @@
 import serviceRepository from "../repository/serviceRepository.js";
-import httpStatus from "http-status"
+import httpStatus from "http-status";
 
 const createNewService = async (req, res) => {
-    try {
-      const serviceData = req.body;
-  
-      const service = await serviceRepository.createService(
-        serviceData
-      );
-  
-      return res.status(httpStatus.CREATED).json({
-        status: httpStatus.CREATED,
-        message: "New service added  successfully.",
-        data: service,
-      });
-    } catch (error) {
-      return res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
-        status: httpStatus.INTERNAL_SERVER_ERROR,
-        message: error.message,
-      });
-    }
-  };
+  try {
+    const serviceData = req.body;
 
-  export default {createNewService}
+    const service = await serviceRepository.createService(serviceData);
+
+    return res.status(httpStatus.CREATED).json({
+      status: httpStatus.CREATED,
+      message: "New service added  successfully.",
+      data: service,
+    });
+  } catch (error) {
+    return res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
+      status: httpStatus.INTERNAL_SERVER_ERROR,
+      message: error.message,
+    });
+  }
+};
+const getAllServices = async (req, res) => {
+  try {
+    return res.status(httpStatus.CREATED).json({
+      status: httpStatus.CREATED,
+      message: "Services Retrieved  successfully.",
+      data: req.services,
+    });
+  } catch (error) {
+    return res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
+      status: httpStatus.INTERNAL_SERVER_ERROR,
+      message: error.message,
+    });
+  }
+};
+const getSingleServiceById = async (req, res) => {
+  try {
+    return res.status(httpStatus.CREATED).json({
+      status: httpStatus.CREATED,
+      message: "Service retrieved  successfully.",
+      data: req.service,
+    });
+  } catch (error) {
+    return res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
+      status: httpStatus.INTERNAL_SERVER_ERROR,
+      message: error.message,
+    });
+  }
+};
+export default { createNewService, getAllServices, getSingleServiceById };
