c9559437897366b628f38f1632827b999860ee60
Ireme-Nyawe <akimana.inono@gmail.com>
2024-12-04 09:56:01 +0200

    route view services

--- src/routes/serviceRoute.js
+++ src/routes/serviceRoute.js
@@ -2,11 +2,12 @@ import express from "express";
 import { isUserAuthorized } from "../middlewares/authorizationMiddleware.js";
 import { newServiceSchema } from "../modules/service/validation/serviceValidation.js";
 import { bodyValidation } from "../middlewares/validationMiddleware.js";
-import { isSameService } from "../middlewares/serviceMiddleware.js";
+import { isSameService, isServiceExistById, isServicesExist } from "../middlewares/serviceMiddleware.js";
 import serviceController from "../modules/service/controller/serviceController.js";
 
 const serviceRoute = express.Router()
 
 serviceRoute.post("/new",isUserAuthorized,bodyValidation(newServiceSchema),isSameService,serviceController.createNewService)
-serviceRoute.get("/view",isUserAuthorized,isSameService,serviceController.createNewService)
+serviceRoute.get("/view",isUserAuthorized,isServicesExist,serviceController.getAllServices)
+serviceRoute.get("/view/:serviceId",isUserAuthorized,isServiceExistById,serviceController.getSingleServiceById)
 export default serviceRoute
